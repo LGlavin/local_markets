48e1343889aef39c2a3bfb1d802b292a4c8483af
Elizabeth Glavin <eglavin@gmail.com>
2013-11-17 10:38:39 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LocalMarkets::Application.config.secret_key_base = 'ddde476ffd56fbfb7ce743f7eb650df90a378da24ea2004edfe1d7cfe39b802ad15df693e7fed74f4dea188709dd82d05e3a6f16ab871cab1b78af6df445bf82'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LocalMarkets::Application.config.session_store :cookie_store, key: '_local_markets_session'
